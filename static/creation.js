1abcd442bc9265dcb6205cdec42f21664c1329ce
Lapikova Alina <49127281+alinlpkv@users.noreply.github.com>
2024-04-16 13:11:00 +0300

    Add files via upload

---
+++ static/creation.js
@@ -0,0 +1,154 @@
+let tg = window.Telegram.WebApp;
+
+//if (tg.initDataUnsafe == '') {
+//    $('body').empty();
+//}
+
+let user_id = tg?.initDataUnsafe?.user?.id || 342297636;
+//let user_id = tg?.initDataUnsafe?.user?.id;
+
+window.onload = function() {
+			new Rolldate({
+				el: '#date-start',
+				format: 'YYYY-MM-DD',
+				beginYear: 2024,
+				endYear: 2025,
+				lang: {
+                        title: "Выбор даты",
+                        cancel: "Отменить",
+                        confirm: "Применить",
+                        year: "",
+                        month: "",
+                        day: "",
+                    }
+			})
+
+			new Rolldate({
+				el: '#time-start',
+				format: 'hh:mm',
+				lang: {
+                        title: "Выбор времени",
+                        cancel: "Отменить",
+                        confirm: "Применить",
+                        year: "",
+                        month: "",
+                        day: "",
+                    }
+
+			})
+
+			new Rolldate({
+				el: '#date-end',
+				format: 'YYYY-MM-DD',
+				beginYear: 2024,
+				endYear: 2025,
+				lang: {
+                        title: "Выбор даты",
+                        cancel: "Отменить",
+                        confirm: "Применить",
+                        year: "",
+                        month: "",
+                        day: "",
+                    }
+			})
+
+			new Rolldate({
+				el: '#time-end',
+				format: 'hh:mm',
+				lang: {
+                        title: "Выбор времени",
+                        cancel: "Отменить",
+                        confirm: "Применить",
+                        year: "",
+                        month: "",
+                        day: "",
+                    }
+
+			})
+		}
+
+$('#datetimepicker1').datetimepicker({
+    locale: moment.locale('ru')
+});
+
+$('#datetimepicker2').datetimepicker({
+    locale: moment.locale('ru'),
+});
+
+$("#datetimepicker1").on("dp.change", function (e) {
+    $('#datetimepicker1').data("DateTimePicker").minDate(moment().toDate());
+
+    var minDateValue = moment(e.date).add(1, 'minutes');
+    if (!isNaN(minDateValue))  {
+        $('#datetimepicker2').data("DateTimePicker").minDate(minDateValue);
+    }
+
+    var selectedTime1 = moment(e.date);
+    var selectedTime2 = $('#datetimepicker2').data("DateTimePicker").date();
+    if (selectedTime1.isAfter(selectedTime2)) {
+        $('#datetimepicker2').data("DateTimePicker").clear();
+    }
+});
+
+
+function postData(user_data) {
+    return new Promise(function(resolve, reject) {
+        let query = `?user_id=${user_id}&theme=${user_data.theme}&` +
+                    `date_start=${user_data.date_start}&date_end=${user_data.date_end}&` +
+                    `description=${user_data.description}&timezone=${user_data.timezone}`;
+
+        console.log(query);
+//        fetch(`https://ai-bankir-helper.ru/meeting/create${query}`)
+        fetch(`http://localhost:8000/meeting/create${query}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Ошибка при отправке данных на сервер: ' + response.statusText);
+                }
+                return response.text();
+            })
+            .then(data => {
+                resolve(data);
+            })
+            .catch(error => {
+                reject(error);
+            });
+    });
+};
+
+
+$('#create').click(function(event) {
+    event.preventDefault();
+
+    $('#errorMessageDateStart').text('');
+    $('#errorMessageTheme').text('');
+
+    if ($('#date_start').val() === '') {
+        event.preventDefault();
+        $('#errorMessageDateStart').text('Заполните время начала встречи');
+        return;
+    }
+    if ($('#theme').val().trim() === '') {
+        event.preventDefault();
+        $('#errorMessageTheme').text('Заполните тему встречи');
+        return;
+    }
+
+    let meeting_data = {
+        theme: $('#theme').val(),
+        date_start: $('#date_start').val(),
+        date_end: $('#date_end').val() || '',
+        description: $('#description').val() || '',
+        timezone: new Date().getTimezoneOffset()
+    };
+
+    postData(meeting_data).
+        then(function(response) {
+            console.log('Данные успешно отправлены на сервер');
+            $('form')[0].reset();
+            window.location.href = 'https://alinlpkv.github.io/tg_web_app/meeting_app/templates/meeting.html';
+        })
+        .catch(function(error) {
+            console.error('Ошибка при отправке данных на сервер:', error);
+        });
+
+});
---
+++ static/meeting.js
@@ -0,0 +1,40 @@
+let tg = window.Telegram.WebApp;
+tg.expand();
+
+//if (tg.initData == '') {
+//    $('body').empty();
+//}
+
+let user_id = tg.initDataUnsafe.user?.id || 342297636;
+//let user_id = tg.initDataUnsafe.user?.id;
+fetchData(user_id);
+
+
+async function fetchData(user_id) {
+
+    try {
+        const response = await fetch(`https://ai-bankir-helper.ru/meeting/show/${user_id}`);
+        const responseData = await response.json();
+
+        console.log(responseData);
+
+        if (responseData.length == 0) {
+            const deleteElement = document.querySelector("#container");
+            deleteElement.innerHTML = '';
+            $('#noMeeting').text('У Вас пока нет новых встреч');
+            return;
+        }
+
+        $('#noMeeting').text('');
+        $.each(responseData, function(index, meeting) {
+            $('#meetingTable tbody').
+            append('<tr><td>' + meeting.theme + '</td><td>' + meeting.date_start + '</td></tr>');
+        });
+
+    } catch (error) {
+        console.error("Ошибка при получении данных:", error);
+        const deleteElement = document.querySelector("#container");
+        deleteElement.innerHTML = '';
+    }
+};
+
